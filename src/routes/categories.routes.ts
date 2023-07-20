9485607d8919d4777c6876a27a5a703833fdbfb8
Uítalo <uitalo.urss@gmail.com>
2023-07-20 13:03:11 -0300

    validação para não permitir que uma nova categoria com mesmo nome de uma que já exista seja criada

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,11 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+  if (categoryAlreadyExists) {
+    return res.status(400).json({ error: "Categoria já existe" });
+  }
+
   categoriesRepository.create({ name, description });
   return res.status(201).send();
 });
