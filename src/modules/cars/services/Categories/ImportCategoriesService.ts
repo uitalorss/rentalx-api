60731d43b94132a0a67364327f71e6d7118a747d
Uítalo <uitalo.urss@gmail.com>
2023-08-08 21:10:49 -0300

    desabilitando criacao de arquivo temporario para importação de novas categorias

--- src/modules/cars/services/Categories/ImportCategoriesService.ts
+++ src/modules/cars/services/Categories/ImportCategoriesService.ts
@@ -30,6 +30,7 @@ class ImportCategoriesService {
           });
         })
         .on("end", () => {
+          fs.promises.unlink(file.path);
           resolve(categories);
         })
         .on("error", (err) => {
