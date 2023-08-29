9cd1ef2c2e11e0c80f5ac03c75fefe7d2029b221
Uítalo <uitalo.urss@gmail.com>
2023-08-28 22:40:33 -0300

    configuração dos conteiners da aplicação e do postgres

---
+++ src/database/index.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { createConnection, getConnectionOptions } from "typeorm";
+
+interface IOptions {
+  host: string;
+}
+
+getConnectionOptions().then((options) => {
+  const newOptions = options as IOptions;
+  newOptions.host = "database_ignite"; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
+  createConnection({
+    ...options,
+  });
+});
